d5285cd60ba3b307c0bf2e179f5292351d868d46
Kouji <Kouji@DESKTOP-VO9O7C0>
2024-05-24 08:41:16 +0800

    sleep muna

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,18 +1,52 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const InfoBox = ({ text, link, btnText }) => (
+    <div className='info-box'>
+        <p className='font-thin sm:text-2xl text-center'>{text}</p>
+
+        <Link to={link} className='neo-brutalism-white neo-btn'>
+            {btnText}
+        </Link>
+    </div>
+)
 
 const renderContent = {
     1: (
-        <h1>1</h1>
+        <InfoBox
+            text="These comprise the compilations of my completed projects."
+            link='/projects'
+            btnText='See Projects'/>
     ), 
 
     2: (
-        <h1>2</h1>
+        <InfoBox
+            text={
+                <>
+                Interested in contacting me? .
+                <br />
+                Please click the button below.
+                </>
+                }
+            link='/contact'
+            btnText='Contact'/>
     ), 
 
     3: (
-        <h1>3</h1>
+        <InfoBox
+            text={                
+            <>
+                Hello! I am Jecho P. Torrefranca, a Computer Science student.
+                <br />
+                To learn more about me, please click the button below.
+            </>
+            }
+            link='/about'
+            btnText='Learn More'/>
     ), 
 }
+
+
 
 const HomeInfo = ({ currentStage }) => {
   return renderContent[currentStage] || null;
